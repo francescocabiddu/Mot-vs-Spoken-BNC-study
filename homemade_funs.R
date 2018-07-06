a9693961787fb675b55f5232b63c1ca41886de7c
Francesco Cabiddu <cabiddu@hotmail.it>
2018-07-06 13:15:50 +0200

    round_df and function to notify when process is finished

--- homemade_funs.R
+++ homemade_funs.R
@@ -5,3 +5,14 @@ round_df <- function(df, digits) {
   
   (df)
 }
+
+notify_me <- function(subj = "R: Come back to me", pass, recipient) {
+  send.mail(from = "fcabiddur@gmail.com",
+            to = recipient,
+            subject= subj,
+            body = "DO NOT REPLY",
+            smtp = list(host.name = "smtp.gmail.com", port = 465, 
+                        user.name="fcabiddur@gmail.com", passwd=pass, ssl=TRUE),
+            authenticate = TRUE,
+            send = TRUE)
+}
